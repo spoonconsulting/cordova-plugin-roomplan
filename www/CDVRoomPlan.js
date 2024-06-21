a2d4e24c68af6ac2c4213197e91d06c56e04efdc
Zafir Sk Heerah <zafir.heerah@spoonconsulting.com>
2024-06-21 07:19:48 +0400

    On Leave should kill Scan and isSupported

--- www/CDVRoomPlan.js
+++ www/CDVRoomPlan.js
@@ -1,6 +1,11 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.openRoomPlan = function(success, error) {
-    console.log("CDVRoomPlan.js: openRoomPlan");
-    exec(success, error, "CDVRoomPlan", "openRoomPlan", []);
-  };
+exports.openRoomPlan = function (success, error) {
+  console.log("CDVRoomPlan.js: openRoomPlan");
+  exec(success, error, "CDVRoomPlan", "openRoomPlan", []);
+};
+
+exports.isSupported = function (success, error) {
+  console.log("CDVRoomPlan.js: isSupported");
+  exec(success, error, "CDVRoomPlan", "isSupported", []);
+}
